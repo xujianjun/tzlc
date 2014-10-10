ebc9f522fb4a4b5b592f26b1d7e6d2819ab35692
xujj_dzh <271816890@qq.com>
2014-10-10 17:32:39 +0800

    e

--- app/views/widget/panel.volt
+++ app/views/widget/panel.volt
@@ -1,7 +1,8 @@
 
 <div class="panel panel-primary">
     <div class="panel-heading">
-      <h3 class="panel-title"><b>{{ items[0]['title'] }}</b></h3>
+      <div class="panel-title"><b><a href="{{ items[0]['link'] }}">{{ items[0]['title'] }}</a></b></div>
+      <div class="more fright"><a href="{{ items[0]['link'] }}">更多&gt;&gt;</a></div>
     </div>
     <div class="panel-body">
       	<ul class="tl-panel-list list-unstyled">
--- data/cache/volt/e__dzh_vm-gw_tzlc_app_views_widget_panel.volt.php
+++ data/cache/volt/e__dzh_vm-gw_tzlc_app_views_widget_panel.volt.php
@@ -1,7 +1,8 @@
 
 <div class="panel panel-primary">
     <div class="panel-heading">
-      <h3 class="panel-title"><b><?php echo $items[0]['title']; ?></b></h3>
+      <div class="panel-title"><b><a href="<?php echo $items[0]['link']; ?>"><?php echo $items[0]['title']; ?></a></b></div>
+      <div class="more fright"><a href="<?php echo $items[0]['link']; ?>">更多&gt;&gt;</a></div>
     </div>
     <div class="panel-body">
       	<ul class="tl-panel-list list-unstyled">
